551179d225c0e7a15ad8ffbc635b06318d933fa2
Daniel Harper <dan@hrpr.co.uk>
2024-09-24 13:24:03 +0100

    Latest changes for nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const plugin = require('tailwindcss/plugin');
 module.exports = {
   prefix: 'twcss-',
   content: [
@@ -22,5 +23,26 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const containerBreak = {
+        '.container-break': {
+          left: '50%',
+          'margin-left': '-50vw',
+          'margin-right': '-50vw',
+          'max-width': '100vw',
+          position: 'relative',
+          right: '50%',
+          width: '100vw',
+        },
+        '.container-break-reset': {
+          'margin-left': '0',
+          'margin-right': '0',
+          transform: 'none',
+          width: 'auto',
+        },
+      };
+      addUtilities(containerBreak);
+    }),
+  ],
 };
